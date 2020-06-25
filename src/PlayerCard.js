8de66d7c446a4c0086461ac38ae8554dfdcb656d
unknown <matthewpiedra1228@gmail.com>
2020-06-25 10:35:33 -0400

    Added image source to PlayerCard component

--- src/PlayerCard.js
+++ src/PlayerCard.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
+const scissors = "https://i.imgur.com/pgjyhIZ.png";
+const paper = "https://i.imgur.com/2gsdqvR.png";
+const rock = "https://i.imgur.com/LghSkIw.png"; 
+
 const PlayerCard = (props) => {
-    return (
-     <div className="PlayerCard"> {props.sign} </div>
+  let image = "";
+
+  if(props.sign === 'rock') {
+     image = rock;
+  }
+  else if(props.sign === 'paper') {
+     image = paper;
+  }
+  else {
+     image = scissors;
+  }
+
+  return (
+     <div className="player-card"> <img src={image}/> </div>
     );
 }
 
-export default PlayerCard
+export default PlayerCard;
